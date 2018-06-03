af89681a616beb75773922b7754a0f2a81fde81c
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-02 21:16:34 -0500

    Add log statment

--- src/app.js
+++ src/app.js
@@ -4,5 +4,7 @@
  * The entrypoint to the application.
  */
 
+console.log("Once you POP you just can't stop");
 console.log("Hello, World!");
 console.log("To infinity and BEYOND!");
+console.log("Fin");
