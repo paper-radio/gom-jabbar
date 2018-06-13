6fedf353a6a4903fe62e0137c9518ea6b76e5c6e
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-13 13:09:41 -0500

    Add content to app.js

--- src/app.js
+++ src/app.js
@@ -7,4 +7,21 @@
 console.log("Once you POP you just can't stop");
 console.log("Hello, World!");
 console.log("To infinity and BEYOND!");
+
+// ---- Start of counter example ----
+console.log("\n\n---\n\n");
+function actionCreator() {
+    return {
+        type: "INCREASE_COUNT"
+    };
+}
+
+const anAction = actionCreator();
+
+console.log(
+    "Here is the action made with the action creator function",
+    anAction
+);
+console.log("\n\n---\n\n");
+// ---- End of counter example ----
 console.log("Fin");
