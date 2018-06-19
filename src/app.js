fd8f93a2d464fc2fbbc0e99d25e41b025aa87351
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-18 22:23:49 -0500

    Add gameExample.json

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@
  * The entrypoint to the application.
  * This branch is for learning functional programming.
  */
-const heros = require("../src/data/heros.json");
 
 console.log("\n\n---Start---\n\n");
 
-console.log(heros);
-
 console.log("\n\n---Fin---\n\n");
-
-debugger;
