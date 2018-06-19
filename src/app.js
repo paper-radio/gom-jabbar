01a4dfcf099cea78a87bcc4b3bd6632d3212e852
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-19 02:39:55 -0500

    Clear app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,6 @@
  * The entrypoint to the application.
  */
 
-import store from "./store";
-
 console.log("\n\n\nStart of the app\n\n");
-
-console.log(store.getState());
 
 console.log("\n\nEnd of the app\n");
