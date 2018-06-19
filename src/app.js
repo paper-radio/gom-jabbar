1b330d286403972cc77e5a405a40d5c13e6311a4
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-18 21:04:36 -0500

    Reset app.js to start taking notes

--- src/app.js
+++ src/app.js
@@ -2,26 +2,9 @@
  * app.js
  *
  * The entrypoint to the application.
+ * This branch is for learning functional programming.
  */
 
-console.log("Once you POP you just can't stop");
-console.log("Hello, World!");
-console.log("To infinity and BEYOND!");
+console.log("Start");
 
-// ---- Start of counter example ----
-console.log("\n\n---\n\n");
-function actionCreator() {
-    return {
-        type: "INCREASE_COUNT"
-    };
-}
-
-const anAction = actionCreator();
-
-console.log(
-    "Here is the action made with the action creator function",
-    anAction
-);
-console.log("\n\n---\n\n");
-// ---- End of counter example ----
 console.log("Fin");
