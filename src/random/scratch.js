a28f37e24a2fdc3ae77d25943b4e5a8a1835f768
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-18 21:48:37 -0500

    Make functional learning scratch pad

---
+++ src/random/scratch.js
@@ -0,0 +1,10 @@
+const people = ["Alex", "Jordan", "Gabe"];
+
+people;
+// Don't use push... use concat
+
+const multiply = (a, b) => a * b;
+const sum = (a, b) => a + b;
+const divide = (n, d) => n / d;
+const squared = n => multiply(n, n);
+const double = n => multiply(n, 2);
