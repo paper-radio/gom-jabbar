21bf0573424add71a22745e723e262d329e8d7b7
Alexander Adrian Guerra <alex@paper-rad.io>
2018-06-18 22:08:49 -0500

    Playing around

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@
  * The entrypoint to the application.
  * This branch is for learning functional programming.
  */
+const heros = require("../src/data/heros.json");
 
 console.log("\n\n---Start---\n\n");
+
+console.log(heros);
 
 console.log("\n\n---Fin---\n\n");
 
--- src/random/scratch.js
+++ src/random/scratch.js
@@ -8,3 +8,19 @@ const sum = (a, b) => a + b;
 const divide = (n, d) => n / d;
 const squared = n => multiply(n, n);
 const double = n => multiply(n, 2);
+
+// Kissing
+
+const kiss = a => {
+    return a !== "frog" ? a : "prince";
+};
+
+function transform(thing, changFunction) {
+    return changFunction(thing);
+}
+
+const frog = transform("frog", kiss);
+frog;
+
+const theSky = transform("the sky", kiss);
+theSky;
